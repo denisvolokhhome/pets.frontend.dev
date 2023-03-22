a399152cc902a99eca7a8b543f06edd013b28cd2
Denis Volokh <dvolokh@corepartners.com>
2023-03-21 22:08:45 -0400

    added toastr  and register form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TopMenuComponent } from './components/top-menu/top-menu.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { RegisterComponent } from './components/register/register.component';
     TopMenuComponent,
     LoginComponent,
     RegisterComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  constructor(private builder:FormBuilder, private toastr:ToastrService){
+  }
+
+  registerForm = this.builder.group({
+    id:this.builder.control('', Validators.compose([Validators.required, Validators.minLength(5)])),
+    name:this.builder.control('', Validators.required),
+    email:this.builder.control('', Validators.compose([Validators.required, Validators.email])),
+    password:this.builder.control('', Validators.compose([Validators.required , Validators.pattern("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")])),
+    password_confirmation:this.builder.control('', Validators.required),
+    role:this.builder.control(''),
+    isActive:this.builder.control(false)
+  })
+
+  proceedRegistration(){
+    if(this.registerForm.valid){
+
+    }else{
+      this.toastr.warning('Please enter valid data');
+    }
+
+
+  }
+
 
 }
