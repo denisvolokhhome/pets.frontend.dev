036db682a7590c323ba4a93d385da8dbc0759841
Denis Volokh <dvolokh@corepartners.com>
2023-04-18 22:01:21 -0400

    Revert "Loading message added"
    This reverts commit 81b566ef3b24ae82548b6189926978683864c162.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { AfterViewInit, Component, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  constructor(private renderer: Renderer2) {}
+export class AppComponent {
   title = 'pets.frontend.dev';
-  ngAfterViewInit() {
-    let loader = this.renderer.selectRootElement('#loader');
-    this.renderer.setStyle(loader, 'display', 'none');
-  }
 }
