81b566ef3b24ae82548b6189926978683864c162
Denis Volokh <dvolokh@corepartners.com>
2023-04-18 22:00:27 -0400

    Loading message added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  constructor(private renderer: Renderer2) {}
   title = 'pets.frontend.dev';
+  ngAfterViewInit() {
+    let loader = this.renderer.selectRootElement('#loader');
+    this.renderer.setStyle(loader, 'display', 'none');
+  }
 }
